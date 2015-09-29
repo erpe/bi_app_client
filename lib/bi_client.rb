7dfc943697c7b9915538e910dbbff9fb78f24307
rene paulokat <rene@so36.net>
2015-09-29 17:20:57 +0200

    config

--- lib/bi_client.rb
+++ lib/bi_client.rb
@@ -2,28 +2,36 @@ require 'net/http'
 require 'json'
 
 module BI
-  module Config
-    extend self
+  class << self 
+    attr_accessor :configuration
+  end
+  
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
 
+  class Configuration
     attr_accessor :api_key
     attr_accessor :reporter_id
+    attr_accessor :bi_base_url
+    attr_accessor :api_version
 
-    def config(&block)
-     instance_eval &block
-     ##yield self if block_given?
-    end
 
-    def print_config
-      puts api_key
-      puts reporter_id
+    def initialize
+      @api_version = 1
+      @api_key = 0
+      @reporter_id = 0
     end
-    
   end
 
   module Action
     private
     def post_event(args)
       puts "get called #{args}" 
+      puts "api: #{BI.configuration.api_key}\n
+      reporter: #{BI.configuration.reporter_id}\n
+      url: #{BI.configuration.bi_base_url}"
     end
 
     def post_uniq(args)
@@ -33,19 +41,24 @@ module BI
 
   class Client
     include Action
-    include Config
+
+    def initialize
+      if BI.configuration.bi_base_url == nil
+        raise "missing config attr 'bi_base_url'"
+      end
+    end
 
     # required keys: category
     # optional keys: referrer|organisation|revenue|comment
     #
-    def create_event(args)
+    def report_event(args)
       referrer = args.fetch(:referrer, nil)
       organisation = args.fetch(:organisation, nil)
       revenue = args.fetch(:revenue, 0)
       post_event(args)
     end
 
-    def create_uniq(args)
+    def report_uniq(args)
       post_uniq(args)
     end
 
@@ -61,7 +74,6 @@ module BI
         comment: nil
       }
     end
-
     
   end
 end
